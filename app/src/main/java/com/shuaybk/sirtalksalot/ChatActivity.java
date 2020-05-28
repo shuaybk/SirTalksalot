8b582117c2b531c0478c93b68d16890b91a4ad16
Shuayb Khan <shuaybk@gmail.com>
2020-05-28 16:33:25 -0400

    Add menu button for adding contact

--- app/src/main/java/com/shuaybk/sirtalksalot/ChatActivity.java
+++ app/src/main/java/com/shuaybk/sirtalksalot/ChatActivity.java
@@ -1,11 +1,19 @@
 package com.shuaybk.sirtalksalot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.shuaybk.sirtalksalot.databinding.ActivityChatBinding;
@@ -29,5 +37,34 @@ public class ChatActivity extends AppCompatActivity {
 
         FirebaseUser currUser = fbAuth.getCurrentUser();
         mBinding.usernameText.setText("User is: uid=" + currUser.getUid() + ", display name = " + currUser.getDisplayName());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chat_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.sign_out_menu_item) {
+            signOut();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut() {
+        //Display loading UI here!
+        AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Intent intent = new Intent(ChatActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
